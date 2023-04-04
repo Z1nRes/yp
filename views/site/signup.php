507e293fa473edd2cbed2d5a36b0359922e7ec45
Z1nRes <tv.noob@bk.ru>
2023-04-04 09:15:11 +0300

    rework register by role

--- views/site/signup.php
+++ views/site/signup.php
@@ -1,25 +1,31 @@
-<div class="text-center d-flex justify-content-center align-items-center" style="height: 80vh">
-   <form method="post" style="min-width: 250px;">
-      <div id="errors" class="text-danger">
-            <?= $message ?? ''; ?>
-      </div>
-      <h1 class="h3 mb-3 fw-normal">Регистрация</h1>
+<?php
+if ($_SESSION['role'] === 2):
+   ?>
 
-      <select class="form-select" name="role">
-         <option selected>Роль</option>
-         <option value="1">Админ</option>
-         <option value="2">Суперпользователь</option>
-      </select>
-      <div class="form-floating mt-2">
-         <input type="text" class="form-control" id="floatingInput" placeholder="Логин" name="login">
-         <label for="floatingInput">Логин</label>
-      </div>
-      <div class="form-floating mt-2">
-         <input type="password" class="form-control" id="floatingPassword" placeholder="Пароль" name="password">
-         <label for="floatingPassword">Пароль</label>
-      </div>
+   <div class="text-center d-flex justify-content-center align-items-center" style="height: 80vh">
+      <form method="post" style="min-width: 250px;">
+         <div id="errors" class="text-danger">
+               <?= $message ?? ''; ?>
+         </div>
+         <h1 class="h3 mb-3 fw-normal">Регистрация</h1>
 
-      <button class="w-100 btn btn-lg btn-primary mt-3" type="submit">Войти</button>
-   </form>
-   
-</div>
+         <select class="form-select" name="role">
+            <option selected>Роль</option>
+            <option value="1">Админ</option>
+            <option value="2">Суперпользователь</option>
+         </select>
+         <div class="form-floating mt-2">
+            <input type="text" class="form-control" id="floatingInput" placeholder="Логин" name="login">
+            <label for="floatingInput">Логин</label>
+         </div>
+         <div class="form-floating mt-2">
+            <input type="password" class="form-control" id="floatingPassword" placeholder="Пароль" name="password">
+            <label for="floatingPassword">Пароль</label>
+         </div>
+
+         <button class="w-100 btn btn-lg btn-primary mt-3" type="submit">Войти</button>
+      </form>
+      
+   </div>
+
+<?php endif; ?>
